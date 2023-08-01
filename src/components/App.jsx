ba0f9b7c837d06992104561622db2ef63db6e4e5
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-08-01 22:06:38 +0300

    hw 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,93 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './Contactlist/Contactlist';
+import Filter from './Filter/Filter';
+import { nanoid } from 'nanoid';
+// const contactsKey = 'contacts';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+
+    searchQuery: '',
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('App componentDidUpdate');
+
+    if (this.state.contacts !== prevState.contacts) {
+      console.log('Обнловилось поле контакс');
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({
+        contacts: parsedContacts,
+      });
+    }
+  }
+
+  addContact = data => {
+    const finedNumber = this.state.contacts.find(
+      contact => contact.number.toLowerCase() === data.number.toLowerCase()
+    );
+    if (finedNumber) {
+      alert(
+        `In your phoneBook already have this number his name is ${finedNumber.name}`
+      );
+      return;
+    }
+    const newContact = {
+      ...data,
+      id: nanoid(),
+    };
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  onChange = ({ target }) => {
+    const normalizedValue = target.value.trim().toLowerCase();
+
+    this.setState({
+      searchQuery: normalizedValue,
+    });
+  };
+
+  getFilteredContacts = () => {
+    const normalizedFilter = this.state.searchQuery.trim().toLowerCase();
+    return this.state.contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  deliteElement = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const filteredContacts = this.getFilteredContacts();
+    return (
+      <>
+        <ContactForm addContact={this.addContact} />
+        <Filter onChange={this.onChange} searchQuery={this.state.searchQuery} />
+        <ContactList
+          contacts={filteredContacts}
+          deliteElement={this.deliteElement}
+        />
+      </>
+    );
+  }
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,75 @@
+import { Component } from 'react';
+import css from './ContactForm.module.css';
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+
+componentDidMount(){
+  console.log('Mount');
+}
+
+componentDidUpdate(){
+  console.log('Update');
+}
+
+componentWillUnmount(){
+  console.log('Unmount');
+}
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.addContact({
+      name: this.state.name,
+      number: this.state.number,
+    });
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  render() {
+    return (
+      <form className={css.contactForm} action="" onSubmit={this.handleSubmit}>
+        <h2>Name</h2>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          className={css.inputField}
+          onChange={this.handleChange}
+          value={this.state.name}
+        />
+        <h2>Number</h2>
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          className={css.inputField}
+          onChange={this.handleChange}
+          value={this.state.number}
+        />
+        <button type="submit" className={css.submitButton}>
+          Add Contact
+        </button>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
